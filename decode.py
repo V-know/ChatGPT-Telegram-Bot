c7d7db06b2a0b0c60f158e99ae84630ef707adb6
Cancellara <vividone@qq.com>
2023-05-04 15:46:40 +0800

    update decode.py

--- decode.py
+++ decode.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
+
 with open("main.log") as f:
     for line in f.readlines():
-       #print(repr(line))
-       l = json.loads(line)
-       print(f"{l['user']['name']} --- {l['user']['id']} --- {l['prompt']} --- {l['choices'][0]['text']}")
-       print()
-       print("---------------------------------------------------------------------------------------------------")
-       print()
-#print(f"'{' '.join(sys.argv)}'")
-#res = repr(f"'{' '.join(sys.argv)}'")
-#print(res)
+        l = json.loads(line)
+        result = l['choices'][0].get('text') if l['choices'][0].get('text') else l['choices'][0]['message']['content']
+        print(f"{l['user']['name']} --- {l['user']['id']} --- {l['prompt']} --- {result}")
+        print()
+        print("---------------------------------------------------------------------------------------------------")
+        print()
