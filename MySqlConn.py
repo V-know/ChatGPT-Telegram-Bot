cd72e6744a4d391c70fd0fc833cbacff8300e987
Cancellara <vividone@qq.com>
2023-05-11 01:44:49 +0800

    Added user identity-based permissions feature

---
+++ MySqlConn.py
@@ -0,0 +1,193 @@
+# -*- coding: UTF-8 -*-
+"""
+1、执行带参数的ＳＱＬ时，请先用sql语句指定需要输入的条件列表，然后再用tuple/list进行条件批配
+２、在格式ＳＱＬ中不需要使用引号指定数据类型，系统会根据输入参数自动识别
+３、在输入的值中不需要使用转意函数，系统会自动处理
+"""
+
+# from pymysql import cursors
+import pymysql
+import yaml
+from dbutils.pooled_db import PooledDB
+
+# from PooledDB import PooledDB
+
+"""
+Config是一些数据库的配置文件
+"""
+
+# Load data from config.yaml file
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    mysql = config["MYSQL"]
+
+
+class Mysql(object):
+    """
+    MYSQL数据库对象，负责产生数据库连接 , 此类中的连接采用连接池实现获取连接对象：conn = Mysql.getConn()
+            释放连接对象;conn.close()或del conn
+    """
+    # 连接池对象
+    __pool = None
+
+    def __init__(self):
+        # 数据库构造函数，从连接池中取出连接，并生成操作游标
+        self._conn = Mysql.__getConn()
+        self._cursor = self._conn.cursor()
+
+    @staticmethod
+    def __getConn():
+        """
+        @summary: 静态方法，从连接池中取出连接
+        @return MySQLdb.connection
+        """
+        if Mysql.__pool is None:
+            __pool = PooledDB(creator=pymysql,  # 使用链接数据库的模块
+                              maxconnections=60,  # 连接池允许的最大连接数，0和None表示不限制连接数
+                              mincached=2,  # 初始化时，链接池中至少创建的空闲的链接，0表示不创建
+                              maxcached=5,  # 链接池中最多闲置的链接，0和None不限制
+                              maxshared=3,
+                              # 链接池中最多共享的链接数量，0和None表示全部共享。PS: 无用，因为pymysql和MySQLdb等模块的 threadsafety都为1，所有值无论设置为多少，_maxcached永远为0，所以永远是所有链接都共享。
+                              blocking=True,  # 连接池中如果没有可用连接后，是否阻塞等待。True，等待；False，不等待然后报错
+                              maxusage=None,  # 一个链接最多被重复使用的次数，None表示无限制
+                              setsession=["set global time_zone = '+8:00'", "set time_zone = '+8:00'"],  # 开始会话前执行的命令列表。
+                              ping=0,  # ping MySQL服务端，检查是否服务可用。
+                              host=mysql["DBHOST"],
+                              port=mysql["DBPORT"],
+                              user=mysql["DBUSER"],
+                              password=mysql["DBPWD"],
+                              database=mysql["DBNAME"],
+                              charset=mysql["DBCHAR"],
+                              cursorclass=pymysql.cursors.DictCursor)
+        return __pool.connection()
+
+    def getAll(self, sql, param=None):
+        """
+        @summary: 执行查询，并取出所有结果集
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list(字典对象)/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchall()
+        else:
+            result = False
+        return result
+
+    def getOne(self, sql, param=None):
+        """
+        @summary: 执行查询，并取出第一条
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchone()
+        else:
+            result = False
+        return result
+
+    def getMany(self, sql, num, param=None):
+        """
+        @summary: 执行查询，并取出num条结果
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param num:取得的结果条数
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchmany(num)
+        else:
+            result = False
+        return result
+
+    def insertOne(self, sql, value=None):
+        """
+        @summary: 向数据表插入一条记录
+        @param sql:要插入的ＳＱＬ格式
+        @param value:要插入的记录数据tuple/list
+        @return: insertId 受影响的行数
+        """
+        self._cursor.execute(sql, value)
+        return self.__getInsertId()
+
+    def insertMany(self, sql, values):
+        """
+        @summary: 向数据表插入多条记录
+        @param sql:要插入的ＳＱＬ格式
+        @param values:要插入的记录数据tuple(tuple)/list[list]
+        @return: count 受影响的行数
+        """
+        count = self._cursor.executemany(sql, values)
+        return count
+
+    def __getInsertId(self):
+        """
+        获取当前连接最后一次插入操作生成的id,如果没有则为０
+        """
+        self._cursor.execute("SELECT @@IDENTITY AS id")
+        result = self._cursor.fetchall()
+        return result[0]['id']
+
+    def __query(self, sql, param=None):
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        return count
+
+    def update(self, sql, param=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要更新的  值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def delete(self, sql, param=None):
+        """
+        @summary: 删除数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要删除的条件 值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def begin(self):
+        """
+        @summary: 开启事务
+        """
+        self._conn.autocommit(0)
+
+    def end(self, option='commit'):
+        """
+        @summary: 结束事务
+        """
+        if option == 'commit':
+            self._conn.commit()
+        else:
+            self._conn.rollback()
+
+    def dispose(self, isEnd=1):
+        """
+        @summary: 释放连接池资源
+        """
+        if isEnd == 1:
+            self.end('commit')
+        else:
+            self.end('rollback')
+        self._cursor.close()
+        self._conn.close()
