06a370dc809988589118c78612de3a8c49deb05a
v-know <vividone@qq.com>
2023-05-11 12:06:53 +0000

    optimizae /start

--- main.py
+++ main.py
@@ -185,8 +185,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+        rf"Hej  {user.mention_html()}!",
     )
 
 
