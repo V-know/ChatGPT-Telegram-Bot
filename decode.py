00fa0c93f658928260fe5c8c65264cb7347e6eb1
Cancellara <vividone@qq.com>
2023-05-28 01:16:00 +0800

    Update decode.py

--- decode.py
+++ decode.py
@@ -6,11 +6,13 @@ with open("main.log") as f:
     for line in f.readlines():
         try:
             l = json.loads(line)
-            result = l['choices'][0].get('text') if l['choices'][0].get('text') else l['choices'][0]['message']['content']
+            result = l['choices'][0].get('text') if l['choices'][0].get('text') else l['choices'][0]['message'][
+                'content']
             print(f"{l['user']['name']} --- {l['user']['id']} --- {l['prompt']} --- {result}")
             print()
-            print(i, "---------------------------------------------------------------------------------------------------")
+            print(i,
+                  "---------------------------------------------------------------------------------------------------")
             print()
-            i+=1
+            i += 1
         except (json.decoder.JSONDecodeError, KeyError):
             continue
