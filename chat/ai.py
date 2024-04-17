be85567c652f6f856ebfb81eb69ee6bcae616029
Cancellara <vividone@qq.com>
2024-04-17 22:49:54 +0800

    feat: Update the version of openai to aiohttp==3.9.2 (#45)

--- chat/ai.py
+++ chat/ai.py
@@ -1,5 +1,5 @@
 from config import token
-import openai
+from openai import AzureOpenAI, OpenAI
 from db.MySqlConn import config
 
 OPENAI_CHAT_COMPLETION_OPTIONS = {
@@ -9,32 +9,28 @@ OPENAI_CHAT_COMPLETION_OPTIONS = {
     "presence_penalty": 0,
     "stream": True,
     "stop": None,
-    "model": "gpt-3.5-turbo" if config["AI"]["TYPE"] != "azure" else None,
-    "engine": None if config["AI"]["TYPE"] != "azure" else config["AI"]["ENGINE"]
+    "model": config["AI"]["MODEL"]
 }
 
 
 async def ChatCompletionsAI(logged_in_user, messages) -> (str, str):
     level = logged_in_user.get("level")
-
-    # Setup AI
-    openai.api_key = config["AI"]["TOKEN"]
-
-    if config["AI"]["TYPE"] == "azure":
-        openai.api_type = config["AI"]["TYPE"]
-        openai.api_base = config["AI"]["BASE"]
-        openai.api_version = config["AI"]["VERSION"]
-    else:
-        OPENAI_CHAT_COMPLETION_OPTIONS["model"] = config["AI"]["MODEL"]
-        openai.api_base = config["AI"]["BASE"]
-
-    response = await openai.ChatCompletion.acreate(
-        messages=messages,
-        max_tokens=token[level],
-        **OPENAI_CHAT_COMPLETION_OPTIONS)
+    azureOpenAIConfig = {'api_key': config["AI"]["TOKEN"],
+                         'azure_endpoint': config["AI"]["BASE"],
+                         'api_version': config["AI"]["VERSION"]}
 
     answer = ""
-    async for r in response:
-        delta = r.choices[0].delta
-        answer += delta["content"] if delta.get("content") else ""
-        yield answer, r.choices[0]["finish_reason"]
+    if config["AI"]["TYPE"] == "azure":
+        client = AzureOpenAI(**azureOpenAIConfig)
+    else:
+        client = OpenAI(**azureOpenAIConfig)
+    with client.chat.completions.with_streaming_response.create(
+            messages=messages,
+            max_tokens=token[level],
+            **OPENAI_CHAT_COMPLETION_OPTIONS) as response:
+        for r in response.parse():
+            if r.choices:
+                delta = r.choices[0].delta
+                if delta.content:
+                    answer += delta.content
+                yield answer, r.choices[0].finish_reason
